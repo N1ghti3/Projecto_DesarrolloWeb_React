fc13156823119fdc6db9862eb4ef87df631ddcef
Nightie Dev <jhonortiz6000@gmail.com>
2025-10-22 11:40:31 -0500

    feat(bar): add initial orders data and styles for Bar Display View

---
+++ verve-os/src/assets/initialData/initialDataBarDisplayView.js
@@ -0,0 +1,8 @@
+// Datos inniciales para las órdenes del bar
+
+const initialOrders = [
+  { id: 101, table: 5, item: 'Margarita', status: 'recibido' },
+  { id: 102, table: 2, item: 'Cerveza Club Colombia', status: 'recibido' },
+];
+
+export default initialOrders;
